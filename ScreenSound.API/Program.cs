fa7f5be7d8ffb3e371e1df73ad6a20ea0d30db59
Claudio Daniel <danielperuna2012@gmail.com>
2025-07-11 15:35:37 -0300

    feat: Refatora o gerenciamento de artistas para usar injeção de dependência

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -2,17 +2,20 @@ using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using ScreenSound.Shared.Modelos;
+using System.Data.SqlTypes;
 using System.Reflection.Metadata.Ecma335;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); 
 var app = builder.Build();
 
-app.MapGet("/Artistas", () =>
+app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     var listaDeArtistas = dal.Listar();
     if (listaDeArtistas is null)
     {
@@ -22,9 +25,8 @@ app.MapGet("/Artistas", () =>
     return Results.Ok(listaDeArtistasResponse);
 });
 
-app.MapGet("/Artistas/{nome}", (string nome) =>
+app.MapGet("/Artistas/{nome}", ([FromServices] DAL < Artista > dal, string nome) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
     if (artista is null)
     {
@@ -34,8 +36,7 @@ app.MapGet("/Artistas/{nome}", (string nome) =>
     return Results.Ok(artistaResponse);
 });
 
-app.MapPost("/Artistas", ([FromBody]Artista artista) => {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
+app.MapPost("/Artistas", ([FromServices] DAL < Artista > dal, [FromBody]Artista artista) => {
     dal.Adicionar(artista);
     return Results.Ok();
 });

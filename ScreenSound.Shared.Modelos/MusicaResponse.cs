9222aded1926d903d6f3449a7dc324e548df225a
Claudio Daniel <danielperuna2012@gmail.com>
2025-07-11 16:40:08 -0300

    feat: Adiciona endpoints para gerenciamento de músicas na API

---
+++ ScreenSound.Shared.Modelos/MusicaResponse.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Shared.Modelos
+{
+    public record MusicaResponse(int Id, string Nome, int? AnoLancamento, Artista? Artista);
+}
